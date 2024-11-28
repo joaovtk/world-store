4fe0a19ea5ea117242038846e63188b8bd562897
Claudio Silva <csclaudiosilva2@gmail.com>
2024-11-28 14:15:59 -0300

    Create Project

---
+++ src/main/java/app/world/store/api/worldstore/WorldstoreApplication.java
@@ -0,0 +1,13 @@
+package app.world.store.api.worldstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorldstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/world/store/api/worldstore/WorldstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package app.world.store.api.worldstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorldstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
